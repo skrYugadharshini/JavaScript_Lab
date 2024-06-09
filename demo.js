e876c7a7c7e5e6f85a55551988bea9906f0f4aa6
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-06-09 21:26:43 +0530

    basic

---
+++ demo.js
@@ -0,0 +1,12 @@
+var factor="yuga"
+var fplayer="riznee"
+var fmovie="kushi"
+
+function favourite()
+{
+    console.log("fav actor:"+factor)
+    console.log("fav player:"+fplayer)
+    console.log("fav movie:"+fmovie)
+}
+
+favourite()
