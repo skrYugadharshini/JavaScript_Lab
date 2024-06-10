84ba586fa1b4246196710d92d64cad8a5c350801
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-06-10 18:50:34 +0530

    basic

--- demo.js
+++ demo.js
@@ -1,15 +1,8 @@
-/html>
-    </body>
-</html>
-    </body>
-</html>\
-<!DOCTYPE html>
-<html lang=""en>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
-        <meta name="viewport" content="initial-scale=1.0">
-        <link rel="stysheet" href="style.css">
-        <tittle>js</tittle>
-
-    </head>
+var rain =true
+if(rain)
+    {
+        console.log("take an umbrella")
+    }
+    else{
+        console.log("Enjoy the sunshine")
+    }
