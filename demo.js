4addde6366d8cfdfbbc61cc3227fad529262bc03
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-06-10 19:37:09 +0530

    basic

--- demo.js
+++ demo.js
@@ -111,3 +111,30 @@ if(rain)
         <meta http-equiv="X-UA-Comaptible" content="IE-edge">
         <meta name="viewport" content="initial-scale=1.0">
         <link re
+
+!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link re
