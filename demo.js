4749edaa93e7f9b27c7d385c4e2c83ed441ce72f
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-06-11 20:30:20 +0530

    basic

--- demo.js
+++ demo.js
@@ -567,3 +567,84 @@ ello')
         <meta http-equiv="X-UA-Comaptible" content="IE-edge">
         <meta name="viewport" content="initial-scale=1.0">
         <link re
+        !DOCTYPE html>
+        <html lang=""en>
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+                <meta name="viewport" content="initial-scale=1.0">
+                <link rel="stysheet" href="style.css">
+                <tittle>js</tittle>
+        
+            </head>
+            <body>
+                <script>
+                    const =
+                    console.log('hello')
+                </script>
+        
+        
+            </body>
+        </html>
+        <!DOCTYPE html>
+        <html lang=""en>
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+                <meta name="viewport" content="initial-scale=1.0">
+                <link re
+
+!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link re
+
+!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link re
+
