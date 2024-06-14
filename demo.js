a853f6ca59b9ae9cb4774679bc208683b9e6631a
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-06-14 20:10:38 +0530

    html

--- demo.js
+++ demo.js
@@ -1,6 +1,44 @@
-var score = 40
+</html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
 
-if(70<50)
-    {
-        console.log("you need to improve")
-    }
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head></head>
