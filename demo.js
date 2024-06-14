c86eca30f5b6f4e0249bd94534130ea6e2c4778a
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-06-14 20:27:45 +0530

    html

--- demo.js
+++ demo.js
@@ -14,7 +14,52 @@
             const =
             console.log('hello')
         </script>
+        </html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
 
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <AbortController<>
+            </>
 
     </body>
 </html>
