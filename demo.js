dabf2c489a5ac6942a5789ad24d29375a7cd452e
skrYugadharshini <139324619+skrYugadharshini@users.noreply.github.com>
2024-06-15 21:08:44 +0530

    html

--- demo.js
+++ demo.js
@@ -2656,3 +2656,232 @@ d>
 <html lang=""en>
     <head>
     </head>
+    d>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+        </html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+    d>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+        </html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+    d>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+        </html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+    d>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+        </html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+    d>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+        </html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+    d>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+        </html>
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Comaptible" content="IE-edge">
+        <meta name="viewport" content="initial-scale=1.0">
+        <link rel="stysheet" href="style.css">
+        <tittle>js</tittle>
+
+    </head>
+    <body>
+        <script>
+            const =
+            console.log('hello')
+        </script>
+
+
+    </body>
+</html>
+<!DOCTYPE html>
+<html lang=""en
+<!DOCTYPE html>
+<html lang=""en>
+    <head>
+    </head>
